f63eefa7da7f2d368e9d68dd2ba17c215bdecafe
chrishah <chrisi.hahni@gmail.com>
2020-06-23 11:23:57 +0200

    add cleanup function, to renumber and drop those with empty values

--- bin/split_fasta.py
+++ bin/split_fasta.py
@@ -35,9 +35,22 @@ def distribute(fh, n, dic):
         dic[target] += ">%s\n%s\n" %(r.id, str(r.seq))
         counter+=1
 
+def cleanup(dic):
+    """ 
+    rename the keys in the dictionary with leading zeros, in the process removing empty elements (happens when actual number of contigs is smaller than n)
+    """
+    counter = 1
+    keys = dic.keys()
+    for b in keys:
+        if dic[b]:
+            dic["%04d"%counter] = dic.pop(b)
+            counter+=1
+        else:
+            del(dic[b])
+        
 def write(dic):
     for b in dic.keys():
-        fh = open("%04d"%b+".fasta", "w")
+        fh = open(b+".fasta", "w")
         fh.write(dic[b])
         fh.close()
 
@@ -63,8 +76,11 @@ if __name__ == '__main__':
     else:
 	fh = open(fasta)
 
-    d=setup_dic(n)
+    d = setup_dic(n)
     distribute(fh, n, d)
+#    print d.keys()
+    cleanup(d)
+#    print d.keys()
     write(d)
 
 #cat <(echo -e "50") ../Eubothrium/assembly/PLATANUS/Ecr_Fus_St-wr/Ecr_Fus_St-wr_scaffold.fa | perl -ne 'chomp; if ($. == 1){$n = $_}else{if ($_=~ />/){if ($s){open(FH, ">>", sprintf("%04d", int(rand($n)+1)).".fasta"); print FH "$h\n$s\n"; close(FH)}; $h=$_}else{$s.=$_}}'
