b496ba24b0a2dac4b3a4cf21271fd85382fcca64
Philipp Resl <resl.philipp@gmail.com>
2020-09-15 14:44:37 +0200

    Fix Sauron MPI config

--- bin/immediate_submit.py
+++ bin/immediate_submit.py
@@ -71,7 +71,7 @@ elif subs == "sge":
 	cmdline = ["qsub"]
 	
 	# TODO: add correct thread handling for SGE clusters
-	sge_args = "-cwd -V -q {queue} -l h_vmem={mem} -pe {queue} {ntasks} -o {output} -e {error} -N {N}".format(**job_properties["cluster"])	
+	sge_args = "-cwd -V -q {queue} -l h_vmem={mem} -pe {pe} {ntasks} -o {output} -e {error} -N {N}".format(**job_properties["cluster"])	
 	cmdline.append(sge_args)
 
 	#now work on dependencies
