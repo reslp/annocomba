878be97186ae75831fd0a09b8e3a4a47de324a7a
chrishah <chrisi.hahni@gmail.com>
2020-10-14 23:34:19 +0200

    make Repeatmasker and maker setup cleaner

--- bin/setup_Repeatmasker.sh
+++ bin/setup_Repeatmasker.sh
@@ -10,17 +10,23 @@ cp -pfr /usr/local/RepeatMasker $destination
 #cd $destination/RepeatMasker
 #perl ./rebuild
 #cd -
-tar xvfz $repeattarball -C $destination/RepeatMasker/
-
-#This is a custom step that checks the metadata against the actual sequence data and removes those that are not present in the metadata
-cd $destination/RepeatMasker
-echo "Checking Repbase metadata against sequence data"
-cat Libraries/RMRBSeqs.embl | grep "^ID " | sed 's/^ID   //' | cut -d " " -f 1 | sort -n | uniq | perl -ne 'chomp; print "$_\n$_\n"' > comp1
-cat Libraries/RMRBMeta.embl | grep "^ID " | sed 's/^ID   //' | cut -d " " -f 1 | sort -n | uniq > comp2
-cat comp1 comp2 | sort -n | uniq -c | grep "   1 " | sed 's/^ .*1 //' > missing
-for m in $(cat missing); do sed -i "/$m/,/\/\//d" Libraries/RMRBMeta.embl; done
-rm comp1 comp2 missing
-
+if [ -f "$repeattarball" ]
+then
+	echo -ne "Extracting Repbase Repatmasker library .. "
+	tar xfz $repeattarball -C $destination/RepeatMasker/
+	echo "Done!"
+	#This is a custom step that checks the metadata against the actual sequence data and removes those that are not present in the metadata
+	cd $destination/RepeatMasker
+	echo -ne "Checking Repbase metadata against sequence data .. "
+	cat Libraries/RMRBSeqs.embl | grep "^ID " | sed 's/^ID   //' | cut -d " " -f 1 | sort -n | uniq | perl -ne 'chomp; print "$_\n$_\n"' > comp1
+	cat Libraries/RMRBMeta.embl | grep "^ID " | sed 's/^ID   //' | cut -d " " -f 1 | sort -n | uniq > comp2
+	cat comp1 comp2 | sort -n | uniq -c | grep "   1 " | sed 's/^ .*1 //' > missing
+	for m in $(cat missing); do sed -i "/$m/,/\/\//d" Libraries/RMRBMeta.embl; done
+	rm comp1 comp2 missing
+	cd - &> /dev/null
+	echo "all good!"
+fi
 #BUild the repeatdatabases
+cd $destination/RepeatMasker
 perl ./rebuild
 
--- rules/setup_maker.smk
+++ rules/setup_maker.smk
@@ -7,29 +7,29 @@ rule setup_maker:
 	singularity:
 		"docker://chrishah/premaker-plus:18"
 	output: 
-		bin = directory("bin/maker/bin")
+		bin = directory("bin/maker/bin"),
+		repeatmasker_ok = "bin/RepeatMasker/repeatmasker.ok"
 	shell:
 		"""
 		echo -e "\n$(date)\tStarting on host: $(hostname) ...\n"
 		basedir=$(pwd)
 
-		#Copy the RepeatMasker directory from the image
-		cp -pfrv /usr/local/RepeatMasker bin/
-
 		if [ "{params.repbase}" == "None" ]
 		then
-			echo -e "No additional Repeatlibrary provided - ok"
+			echo -e "No Repbase Repeatmasker library provided - ok"
 		else
-			bin/setup_Repeatmasker.sh bin/ {params.repbase}
-#			tar xvfz {params.repbase} -C bin/RepeatMasker/
-#			perl bin/RepeatMasker/rebuild
+			echo -e "Repbase Repatmasker library provided: {params.repbase} - ok"
 		fi
+		echo -e "Setting up RepeatMasker in '$(pwd)/bin/RepeatMasker/'"
+		bin/setup_Repeatmasker.sh bin/ {params.repbase}
+		touch {output.repeatmasker_ok}
 
 		if [ "{input.maker_tarball}" == "None" ]
 		then
 			echo -e "Providing a maker tarball is mandatory"
 			exit 1
 		else
+			echo -e "\nSetting up maker in '$(pwd)/bin/maker/'"
 			bash bin/setup_maker.sh {input.maker_tarball} bin 
 		fi
 	"""
