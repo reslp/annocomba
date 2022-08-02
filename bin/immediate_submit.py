c5ae6a2922f6a101db35cf73c90d2bb7322c584b
chrishah <chrisi.hahni@gmail.com>
2022-08-02 23:12:37 +0200

    update

--- bin/immediate_submit.py
+++ bin/immediate_submit.py
@@ -38,6 +38,19 @@ sample = ""
 if "sample" in job_properties["wildcards"]:
         sample = job_properties["wildcards"]["sample"]
 
+#add other wildcards to the sample name, e.g. library name 
+if "lib" in job_properties["wildcards"]:
+	sample = sample+"-"+job_properties["wildcards"]["lib"]
+
+#if "basecaller" in job_properties["wildcards"]:
+#	sample = sample+"-"+job_properties["wildcards"]["basecaller"]
+
+#if "longcor" in job_properties["wildcards"]:
+#	sample = sample+"-"+job_properties["wildcards"]["longcor"]
+
+#if "k" in job_properties["wildcards"]:
+#	sample = sample+"-k"+job_properties["wildcards"]["k"]
+
 #add a unit to the sample name (assumes that there is a wildcard 'unit', in our snakefile this is the number of the partitions)
 if "unit" in job_properties["wildcards"]:
 	sample = sample+"-"+job_properties["wildcards"]["unit"]
@@ -45,6 +58,7 @@ if "unit" in job_properties["wildcards"]:
 #add a aed to the sample name (assumes that there is a wildcard 'aed', in our snakefile this is relevant for the Augustus runs)
 if "aed" in job_properties["wildcards"]:
 	sample = sample+"-"+job_properties["wildcards"]["aed"]
+
 
 cmdline=[]
 # create list with command line arguments
@@ -57,11 +71,33 @@ if subs == "slurm":
                 job_properties["cluster"]["error"] = job_properties["cluster"]["error"].replace("slurm", prefix)
 	
 	#determine threads from the Snakemake profile, i.e. as determined in the Snakefile and the main config file respectively
-	job_properties["cluster"]["ntasks"] = job_properties["threads"]
-	job_properties["cluster"]["ntasks-per-node"] = job_properties["threads"]
+	if job_properties["cluster"]["ntasks"] == 1:
+		job_properties["cluster"]["cpus-per-task"] = job_properties["threads"]
+	elif job_properties["cluster"]["ntasks"] == "threads":
+#	job_properties["cluster"]["ntasks-per-node"] = job_properties["cluster"]["ntasks"]
+		job_properties["cluster"]["ntasks"] = job_properties["threads"]
+		job_properties["cluster"]["ntasks-per-node"] = job_properties["cluster"]["ntasks"]
 
+	#determine memory requirements from config file/Snakefile
+	if "params" in job_properties:
+		if "max_mem_in_GB" in job_properties["params"]:
+			job_properties["cluster"]["mem"] = str(job_properties["params"]["max_mem_in_GB"])+"G"
+			print("Setting memory to %s" %job_properties["cluster"]["mem"], file=sys.stderr)
+
+	if "resources" in job_properties:
+		if "mem_gb" in job_properties["resources"]:
+			job_properties["cluster"]["mem"] = str(job_properties["resources"]["mem_gb"])+"G"
+			print("Setting memory to %s" %job_properties["cluster"]["mem"], file=sys.stderr)
+
+	if "n" in job_properties["cluster"] and "N" in job_properties["cluster"]:
+		del job_properties["cluster"]["n"]
+			
 	# create string for slurm submit options for rule
-	slurm_args = "--partition={partition} --time={time} --qos={qos} --ntasks={ntasks} --ntasks-per-node={ntasks-per-node} --hint={hint} --output={output} --error={error} -n {n} -J {J} --mem={mem}".format(**job_properties["cluster"])
+	slurm_args = ""
+	for k in job_properties["cluster"].keys():
+		slurm_args+="--%s=%s " %(k, job_properties["cluster"][k])
+	slurm_args=slurm_args.replace("--N=", "-N ").replace("--J=", "-J ").replace("--n=", "-n ")
+#	slurm_args = "--partition={partition} --time={time} --qos={qos} --ntasks={ntasks} --ntasks-per-node={ntasks-per-node} --ntasks-per-core={ntasks-per-core} --hint={hint} --output={output} --error={error} -n {n} -J {J} --mem={mem}".format(**job_properties["cluster"])
 	cmdline.append(slurm_args)
 	# now work on dependencies
 	if dependencies:
@@ -73,7 +109,7 @@ if subs == "slurm":
 		#print(dependencies, file=sys.stderr)
 elif subs == "sge":
 	if "sample" in job_properties["wildcards"]:
-		name = job_properties["cluster"]["N"] + "_" + sample 
+		name = job_properties["cluster"]["N"] + "_" + sample
 	else:
 		name = job_properties["cluster"]["N"]
 	job_properties["cluster"]["N"] = name
@@ -81,10 +117,16 @@ elif subs == "sge":
 	job_properties["cluster"]["output"] = job_properties["cluster"]["output"].replace("slurm", prefix).replace("%j",name)
 	job_properties["cluster"]["error"] = job_properties["cluster"]["error"].replace("slurm", prefix).replace("%j",name)
 	cmdline = ["qsub"]
-	
+
 	#determine threads from the Snakemake profile, i.e. as determined in the Snakefile and the main config file respectively
 	job_properties["cluster"]["ntasks"] = job_properties["threads"]
+	#determine memory requirements from config file/Snakefile
+	if "params" in job_properties:
+		if "max_mem_in_GB" in job_properties["params"]:
+			job_properties["cluster"]["mem"] = str(job_properties["params"]["max_mem_in_GB"])+"G"
+			print("Setting memory to %s" %job_properties["cluster"]["mem"], file=sys.stderr)
 	
+	# TODO: add correct thread handling for SGE clusters
 	sge_args = "-cwd -V -q {queue} -l h_vmem={mem} -pe {pe} {ntasks} -o {output} -e {error} -N {N}".format(**job_properties["cluster"])	
 	cmdline.append(sge_args)
 
